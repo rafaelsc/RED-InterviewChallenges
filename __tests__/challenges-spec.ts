7292dac284e9c2810fa6f63b2658bbac9de7adfa
Rafael Sliveira Cordeiro <rafaelsc@rafaelsc.net>
2018-03-31 15:42:23 -0700

    Update

--- __tests__/challenges-spec.ts
+++ __tests__/challenges-spec.ts
@@ -15,7 +15,7 @@ test('Challege - #01 - Minute 00 ot 60 should be 0', () => {
 test('Challege - #01 - Right Angles', () => {
   expect(challenge.angleOfMinutes(15)).toBe(90);
   expect(challenge.angleOfMinutes(30)).toBe(180);
-  expect(challenge.angleOfMinutes(45)).toBe(180+90);
+  expect(challenge.angleOfMinutes(45)).toBe(180 + 90);
 });
 
 test('Challege - #01 - Other Angles', () => {
@@ -23,7 +23,6 @@ test('Challege - #01 - Other Angles', () => {
   expect(challenge.angleOfMinutes(59)).toBe(354);
   expect(challenge.angleOfMinutes(31)).toBe(186);
 });
-
 
 test('Challege - #02 - Stock Prices', () => {
   expect(challenge.stockProfit([20, 30])).toBe(10);
@@ -35,5 +34,58 @@ test('Challege - #02 - Stock Prices', () => {
   expect(challenge.stockProfit([0])).toBeUndefined;
   expect(challenge.stockProfit([20, 10])).toBeUndefined;
   expect(challenge.stockProfit([20, 10, 5])).toBeUndefined;
+});
+
+test('Challege - #03 - Twelve Days Of Christmas', () => {
+  challenge.singTheTwelveDaysOfChristmas();
+});
+
+test('Challege - #04 - Reverse String', () => {
+  expect(challenge.reserveWordsInPhrase('Hello')).toBe('Olleh');
+  expect(challenge.reserveWordsInPhrase('ola')).toBe('alo');
+  expect(challenge.reserveWordsInPhrase('UOU')).toBe('UOU');
+  expect(challenge.reserveWordsInPhrase('Hello how are you')).toBe(
+    'Olleh woh era uoy'
+  );
+});
+
+test('Challege - #05 - Is Palindrome', () => {
+  expect(challenge.isPalindrome('racecar')).toBeTruthy;
+  expect(challenge.isPalindrome('a')).toBeTruthy;
+  expect(challenge.isPalindrome('abba')).toBeTruthy;
+  expect(challenge.isPalindrome('Eve')).toBeTruthy;
+
+  expect(challenge.isPalindrome('car')).toBeFalsy;
+  expect(challenge.isPalindrome('')).toBeFalsy;
+});
+
+test('Challege - #05 - pigLatin', () => {
+  expect(challenge.pigLatin('pig')).toBe('igpay');
+  expect(challenge.pigLatin('latin')).toBe('atinlay');
+  expect(challenge.pigLatin('happy')).toBe('appyhay');
+  expect(challenge.pigLatin('banana')).toBe('ananabay');
+
+  expect(challenge.pigLatin('cheers')).toBe('eerschay');
+  expect(challenge.pigLatin('smile')).toBe('ilesmay');
+
+  expect(challenge.pigLatin('eat')).toBe('eatay');
+  expect(challenge.pigLatin('egg')).toBe('eggay');
+});
+
+
+test('Challege - #05 - Range and Sum', () => {
+  expect(challenge.range(1, 10)).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  expect(challenge.range(1, 10, 1)).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  expect(challenge.range(1, 10, 2)).toEqual([1, 3, 5, 7, 9]);
+  expect(challenge.range(5, 2, -1)).toEqual([5, 4, 3, 2]);
+
+  expect(challenge.sum(challenge.range(1, 10))).toBe(55);
+});
+
+test('Challege - #06 - toRGB', () => {
+  expect(challenge.toRGB(1, 1, 1)).toEqual('#ffffff');
+  expect(challenge.toRGB(0, 0, 0)).toEqual('#000000');
+  expect(challenge.toRGB(0, 1, 0)).toEqual('#00ff00');
+  expect(challenge.toRGB(0, 0.914, 0)).toEqual('#00e900');
 });
 
--- src/challenge01.ts
+++ src/challenge01.ts
@@ -34,4 +34,123 @@ export class Challenges {
     });
     return topProfit === 0 ? undefined : topProfit;
   }
+
+  public singTheTwelveDaysOfChristmas():void{
+    var songPattern = [
+      {ordinal: 'First', number: '', present: 'a Partridge in a Pear Tree.'},
+      {ordinal: 'Second', number: 'Two', present: 'Turtle Doves'},
+      {ordinal: 'Third', number: 'Three', present: 'French Hens'},
+      {ordinal: 'Fourth', number: 'Four', present: 'Calling Birds'},
+      {ordinal: 'Fifth', number: 'Five', present: 'Gold Rings'},
+      {ordinal: 'Sixth', number: 'Six', present: 'Geese a-Laying'},
+      {ordinal: 'Seventh', number: 'Seven', present: 'Swans a-Swimming'},
+      {ordinal: 'Eighth', number: 'Eight', present: 'Maids a-Milking'},
+      {ordinal: 'Ninth', number: 'Nine', present: 'Ladies Dancing'},
+      {ordinal: 'Tenth', number: 'Ten', present: 'Lords a-Leaping'},
+      {ordinal: 'Eleventh', number: 'Eleven', present: 'Pipers Piping'},
+      {ordinal: 'Twelfth', number: 'Twelve', present: 'Drummers Drumming'},
+    ];
+
+    var song = "";
+    var presentsAcc = "";
+    var first = true;
+    songPattern.forEach(i => {
+      song += `On the ${i.ordinal} day of Christmas my true love sent to me\n`;
+      presentsAcc = `${i.number === '' ?'' : i.number+' '}${i.present}\n${presentsAcc}`;
+      song += `${presentsAcc}\n`;
+
+      if(first){
+        presentsAcc = `and ${presentsAcc}`;
+      }
+      first = false;
+    });
+
+    console.log(song);
+  }
+
+  public reserveWordsInPhrase(original:String):String{
+    if (original === undefined || original === null){
+      throw new Error('Invalid argument "prices"');
+    }
+
+    var reversePhase = '';
+    var words = original.split(' ');
+    words.forEach(words => {
+      reversePhase += words.split('').reverse().join('') + ' ';
+    });
+
+    //Keep Original Captalization
+    var finalPhase = '';
+    for (let i = 0; i < original.length; i++) {
+      const char = original[i];
+      if(char.match(/([A-Z])/)){
+        finalPhase += reversePhase[i].toLocaleUpperCase();
+      } else {
+        finalPhase += reversePhase[i].toLocaleLowerCase();
+      }
+    }
+    return finalPhase;
+  }
+
+  public isPalindrome(original:String):Boolean{
+    if (original === undefined || original === null){
+      throw new Error('Invalid argument "prices"');
+    }
+    return original.split('').reverse().join('').toLocaleLowerCase() === original.toLocaleLowerCase();
+  }
+
+
+  public pigLatin(original:String):String{
+    if (original === undefined || original === null){
+      throw new Error('Invalid argument "prices"');
+    }
+
+    if(original[0].match(/[aeiou]/gi)){
+      return original+'ay';
+    }
+
+    var lastNonVogalIndex = 0;
+    for (let i = 0; i < original.length; i++) {
+      const char = original[i];
+      lastNonVogalIndex = i;
+      if(original[i].match(/[aeiou]/gi)){
+        break;
+      }
+    }
+    return original.split('').slice(lastNonVogalIndex).join('') + original.split('').slice(0, lastNonVogalIndex).join('') + 'ay';
+  }
+
+  public sum(numbers:number[]):number{
+    var sum = 0;
+    numbers.forEach(n => {
+      sum += n;
+    });
+    return sum;
+  }
+
+  public range(start:number, end:number, step:number = 1):number[]{
+    var array = [];
+
+    array[0]= start;
+    while(step > 0 ? (start + step <= end) : (start + step >= end)){
+      array[array.length] = (start+= step);
+    }
+
+    return array;
+  }
+
+  public toRGB(r:number, g:number, b:number):string{
+    r = Math.max(0, Math.min(r*255, 255));
+    g = Math.max(0, Math.min(g*255, 255));
+    b = Math.max(0, Math.min(b*255, 255));
+    console.log(r,g,b);
+    var rT = Math.floor(r).toString(16);
+    var gT = Math.floor(g).toString(16);
+    var bT = Math.floor(b).toString(16);
+
+    rT = (rT.length == 1 ? '0' : '') + rT;
+    gT = (gT.length == 1 ? '0' : '') + gT;
+    bT = (bT.length == 1 ? '0' : '') + bT;
+    return `#${rT}${gT}${bT}`;
+  }
 }
