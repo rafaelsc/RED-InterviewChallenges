5c4ca1d6030502f4acd755936cd7b72078c189cb
Rafael Sliveira Cordeiro <rafaelsc@rafaelsc.net>
2018-03-31 13:42:35 -0700

    Challege02

--- __tests__/challenges-spec.ts
+++ __tests__/challenges-spec.ts
@@ -7,21 +7,33 @@ test('Chanlend01 - Invlid Entries', () => {
   expect(() => challenge.angleOfMinutes(60)).toThrowError;
 });
 
-test('Chanlend01 - Minute 00 ot 60 should be 0', () => {
+test('Challege - #01 - Minute 00 ot 60 should be 0', () => {
   expect(challenge.angleOfMinutes(0)).toBe(0);
   expect(challenge.angleOfMinutes(60)).toBe(0);
 });
 
-test('Chanlend01 - Right Angles', () => {
+test('Challege - #01 - Right Angles', () => {
   expect(challenge.angleOfMinutes(15)).toBe(90);
   expect(challenge.angleOfMinutes(30)).toBe(180);
   expect(challenge.angleOfMinutes(45)).toBe(180+90);
 });
 
-test('Chanlend01 - Other Angles', () => {
+test('Challege - #01 - Other Angles', () => {
   expect(challenge.angleOfMinutes(20)).toBe(120);
   expect(challenge.angleOfMinutes(59)).toBe(354);
-
   expect(challenge.angleOfMinutes(31)).toBe(186);
+});
+
+
+test('Challege - #02 - Stock Prices', () => {
+  expect(challenge.stockProfit([20, 30])).toBe(10);
+  expect(challenge.stockProfit([20, 30, 10])).toBe(10);
+  expect(challenge.stockProfit([20, 30, 10, 5, 50])).toBe(45);
+  expect(challenge.stockProfit([45, 24, 35, 31, 40, 38, 11])).toBe(16);
+
+  expect(challenge.stockProfit([])).toBeUndefined;
+  expect(challenge.stockProfit([0])).toBeUndefined;
+  expect(challenge.stockProfit([20, 10])).toBeUndefined;
+  expect(challenge.stockProfit([20, 10, 5])).toBeUndefined;
 });
 
--- src/challenge01.ts
+++ src/challenge01.ts
@@ -2,9 +2,7 @@ export class Challenges {
 
   public angleOfMinutes(minuteOfTheCurrentHour: number): number {
     if (minuteOfTheCurrentHour < 0 || minuteOfTheCurrentHour > 60) {
-      throw new RangeError(
-        'Invalid minuteOfTheCurrentHour. Allow only values from 0 to 60'
-      );
+      throw new RangeError('Invalid minuteOfTheCurrentHour. Allow only values from 0 to 60');
     }
     if (minuteOfTheCurrentHour === 0 || minuteOfTheCurrentHour === 60) {
       return 0;
@@ -12,4 +10,28 @@ export class Challenges {
     return (360/60)*minuteOfTheCurrentHour;
   }
 
+  public stockProfit(prices:Array<number>):number | undefined {
+    if (prices === undefined || prices === null){
+      throw new Error('Invalid argument "prices"');
+    }
+    if (prices.length <= 1){
+      return undefined;
+    }
+
+    var min = prices[0];
+    var max = min;
+    var topProfit = 0;
+
+    prices.forEach(price => {
+        if(price > max){
+          max = price;
+          topProfit = Math.max(topProfit, (max - min));
+        }
+        if(price < min){
+          min = price;
+          max = 0;
+        }
+    });
+    return topProfit === 0 ? undefined : topProfit;
+  }
 }
